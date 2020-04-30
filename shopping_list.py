aceee3d4605de8599849109ae6b1d4d5667f05b5
Krzysztof Klamycki <krzkamkla@gmail.com>
2020-04-30 15:22:54 +0200

    Add for loop

--- shopping_list.py
+++ shopping_list.py
@@ -2,3 +2,7 @@ shopping_list = {
     "Piekarnia": ["chleb", "bułki", "pączek", "chałka"],
     "Warzywniak": ["marchew", "seler", "rukola", "pietruszka"]
 }
+
+print("Lista zakupów")
+for place, product in shopping_list.items():
+    print(f"""Idę do {place}, kupuję tu następujące rzeczy: {product}""")
