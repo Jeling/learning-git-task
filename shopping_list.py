d8164951c3c0e92bc31e30ec8f28f483b8dcdf02
Krzysztof Klamycki <krzkamkla@gmail.com>
2020-05-13 15:42:51 +0200

    Add counter

--- shopping_list.py
+++ shopping_list.py
@@ -3,6 +3,10 @@ shopping_list = {
     "Warzywniak": ["marchew", "seler", "rukola", "pietruszka"]
 }
 
+counter = 0
 print("Lista zakupów")
 for place, product in shopping_list.items():
     print(f"""Idę do {place}, kupuję tu następujące rzeczy: {product}""")
+    counter += len(product)
+print(f"""W sumie kupuję {counter} produktów
+""")
